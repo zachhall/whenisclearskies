2cf76818ba005ec3049c844111e1aff179d874da
Zach Hall <zhall@techtalentsouth.com>
2020-01-10 22:54:56 -0500

    First commit, site working

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var now, firstshow, nextMonday, showday, countdownTimer;
+
+firstshow = moment("01/13/2020 6:30:00 PM", "MM-DD-YYYY HH:mm:ss");
+nextshow();
+countdownTimer = setInterval(nextshow, 1000);
+
+function nextshow() {
+	nextMonday = moment().countdown(firstshow).toString();
+	$("#show-countdown").html(nextMonday);
+}
