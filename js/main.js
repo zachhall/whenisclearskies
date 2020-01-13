a1304098d21f45085848bfddf94eff5d9ee00734
Zach Hall <zhall@techtalentsouth.com>
2020-01-12 22:17:11 -0500

    Add moment-timezone and first attempt to use user local timezone for countdown

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var now, firstshow, nextMonday, showday, countdownTimer;
+var now, firstshow, nextMonday, showday, countdownTimer, momentLocal;
 
 firstshow = moment("01/13/2020 18:30:00", "MM-DD-YYYY HH:mm:ss");
 // nextMonday = firstshow.recur().every(["Monday"]).daysofWeek();
@@ -6,9 +6,14 @@ nextshow();
 countdownTimer = setInterval(nextshow, 1000);
 
 function nextshow() {
-	nextMonday = moment().countdown(firstshow).toString();
+	myMoment = moment.utc();
+	momentLocal = myMoment.local();
+	nextMonday = momentLocal.countdown(firstshow).toString();
+
 	$("#show-countdown").html(nextMonday);
+
+	// console.log(momentLocal);
 }
 
-var guessTz = moment().tz.guess(true).countdown(firstshow).toString();
-console.log(guessTz);
+// var guessTz = moment.tz.guess(true);
+// console.log(guessTz);
