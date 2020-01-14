53af7162d1d259ce5e68411683c7b002227998e1
Zach Hall <zhall@techtalentsouth.com>
2020-01-14 10:16:46 -0500

    Move dates up one week

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-var now, firstshow, nextShow, nextMonday, showday, countdownTimer, everyMon;
+var now, firstshow, endShow, nextShow, nextMonday, showday, countdownTimer, everyMon;
 
-firstshow = moment.tz("01/13/2020 18:30:00", "MM-DD-YYYY HH:mm:ss", "America/Los_Angeles");
-nextShow = moment.tz("01/20/2020 18:30:00", "MM-DD-YYYY HH:mm:ss", "America/Los_Angeles");
+firstshow = moment.tz("01/20/2020 18:30:00", "MM-DD-YYYY HH:mm:ss", "America/Los_Angeles");
+endShow = moment.tz("01/20/2020 21:30:00", "MM-DD-YYYY HH:mm:ss", "America/Los_Angeles")
+nextShow = moment.tz("01/27/2020 18:30:00", "MM-DD-YYYY HH:mm:ss", "America/Los_Angeles");
 
 countdownToShow1();
 myMoment = moment();
@@ -10,7 +11,7 @@ if (myMoment.isBefore(firstshow)) {
 	$("div.nonlive").removeClass("hidden");
 	$("div.live").addClass("hidden");
 	countdownTimer = setInterval(countdownToShow1, 1000);
-} else if (myMoment.isAfter(firstshow.add(3, 'h'))) {
+} else if (myMoment.isAfter(firstshow) && myMoment.isBefore(endShow)) {
 	$("div.nonlive").addClass("hidden");
 	$("div.live").removeClass("hidden");
 } else {
